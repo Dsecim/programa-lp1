33f17cccf541a3abfa78d106a76e6fe00f4321b3
Dsecim <48600098+Dsecim@users.noreply.github.com>
2019-04-10 16:35:00 -0300

    Create eh_primo.c

---
+++ eh_primo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  
+int main (void){
+  int num1;
+  printf ("digite um numero:\n");
+  scanf("%d",&num1);
+  int i;
+  for (i=2;i<num1;i++){
+     if (num1%i==0){
+        printf("nao eh primo\n");
+        return 0;
+        break;
+     }
+  }
+  printf("eh primo\n");
+  return 1;
+}
